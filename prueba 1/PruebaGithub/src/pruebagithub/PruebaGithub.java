c3d53008f02ca3b9c863a8f70d4e163980dbf1ec
jack-narv <74697746+jack-narv@users.noreply.github.com>
2021-05-02 21:53:15 -0500

    SALUDO
    Cambio saludo Bryan

--- prueba 1/PruebaGithub/src/pruebagithub/PruebaGithub.java
+++ prueba 1/PruebaGithub/src/pruebagithub/PruebaGithub.java
@@ -18,7 +18,7 @@ public class PruebaGithub {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola jack");
+        System.out.println("Hola Bryan");
         
     }
     
